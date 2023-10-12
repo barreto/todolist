8215477865307cb0370efedfa563eddcc0db260c
Barreto <v.barreto.and@gmail.com>
2023-10-12 12:05:12 -0300

    feat: add repository to save user on create

--- src/main/java/com/barreto/todolist/user/UserController.java
+++ src/main/java/com/barreto/todolist/user/UserController.java
@@ -1,7 +1,7 @@
 package com.barreto.todolist.user;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping
-    public void create(@RequestBody UserModel userModel) throws JsonProcessingException {
+    public UserModel create(@RequestBody UserModel userModel) throws JsonProcessingException {
         System.out.println("Name: " + userModel.getName());
         System.out.println("Username: " + userModel.getUsername());
-        System.out.println(new ObjectMapper().writeValueAsString(userModel));
+
+        return userRepository.save(userModel);
     }
 
 }
