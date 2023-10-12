a6fb918a43bd34e998b8536bdcb6b093c38a2012
Barreto <v.barreto.and@gmail.com>
2023-10-12 20:12:59 -0300

    feat: add credentials validation to control response flux

--- src/main/java/com/barreto/todolist/task/TaskFilterAuth.java
+++ src/main/java/com/barreto/todolist/task/TaskFilterAuth.java
@@ -1,9 +1,13 @@
 package com.barreto.todolist.task;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.barreto.todolist.user.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,26 +17,32 @@ import java.util.Base64;
 @Component
 public class TaskFilterAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         var authorization = request.getHeader("Authorization");
-        System.out.println("auth: " + authorization);
-
         var encodedAuth = authorization.substring("Basic".length()).trim();
-        System.out.println("encodedAuth: " + encodedAuth);
-
         var decodedAuthBuffer = Base64.getDecoder().decode(encodedAuth);
-        System.out.println("decodedAuthBuffer: " + decodedAuthBuffer);
-
         var decodedAuth = new String(decodedAuthBuffer);
-        System.out.println("decodedAuth: " + decodedAuth);
 
         var credentials = decodedAuth.split(":");
         var username = credentials[0];
         var password = credentials[1];
-        System.out.println("username: " + username);
-        System.out.println("password: " + password);
 
-        chain.doFilter(request, response);
+        var user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value());
+        } else {
+            var verificationResult = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+            if (verificationResult.verified) {
+                chain.doFilter(request, response);
+            } else {
+                response.sendError(HttpStatus.UNAUTHORIZED.value());
+            }
+        }
     }
 }
