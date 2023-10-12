561a56ccea30f3cc75de9e672323dbc296fbf56d
Barreto <v.barreto.and@gmail.com>
2023-10-12 19:35:01 -0300

    feat: update filter to use spring that facilitates the development

--- src/main/java/com/barreto/todolist/task/TaskFilterAuth.java
+++ src/main/java/com/barreto/todolist/task/TaskFilterAuth.java
@@ -1,14 +1,18 @@
 package com.barreto.todolist.task;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-public class TaskFilterAuth implements Filter {
+public class TaskFilterAuth extends OncePerRequestFilter {
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         System.out.println("Hello from Filter!");
         chain.doFilter(request, response);
     }
