00298dde9a714154db0838220085991aa086b3fd
Barreto <v.barreto.and@gmail.com>
2023-10-12 13:23:26 -0300

    feat: add task entity

---
+++ src/main/java/com/barreto/todolist/task/TaskModel.java
@@ -0,0 +1,28 @@
+package com.barreto.todolist.task;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 50)
+    private String title;
+    private String description;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
