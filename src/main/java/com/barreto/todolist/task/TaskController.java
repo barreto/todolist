f9cb7dde54f6739157103166099d1357ca76d5a2
Barreto <v.barreto.and@gmail.com>
2023-10-12 21:54:29 -0300

    feat: add date validations in task creation

--- src/main/java/com/barreto/todolist/task/TaskController.java
+++ src/main/java/com/barreto/todolist/task/TaskController.java
@@ -3,11 +3,14 @@ package com.barreto.todolist.task;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -18,10 +21,21 @@ public class TaskController {
     private TaskRepository taskRepository;
 
     @PostMapping
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) throws JsonProcessingException {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) throws JsonProcessingException {
         System.out.println("Hi from Controller!");
         var idUser = (UUID) request.getAttribute("idUser");
         taskModel.setIdUser(idUser);
-        return taskRepository.save(taskModel);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.badRequest().body("Start and end date must be after current date.");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.badRequest().body("Start date must precede end date.");
+        }
+
+        var taskCreated = taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
     }
 }
