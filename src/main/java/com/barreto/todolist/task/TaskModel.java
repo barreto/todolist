65b6be9bb9d8306acdc9a299bca1d2870cea9710
Barreto <v.barreto.and@gmail.com>
2023-10-14 14:04:55 -0300

    feat: add exception handling for http message not readable exceptions

--- src/main/java/com/barreto/todolist/task/TaskModel.java
+++ src/main/java/com/barreto/todolist/task/TaskModel.java
@@ -25,4 +25,12 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("Title field must contains in maximum 50 characters.");
+        }
+
+        this.title = title;
+    }
 }
