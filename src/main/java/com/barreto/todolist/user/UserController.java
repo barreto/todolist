4e6154765171eed0f4994e6a69a7a157205693c2
Barreto <v.barreto.and@gmail.com>
2023-10-26 07:01:47 -0300

    feat: add endpoint to find user by id

--- src/main/java/com/barreto/todolist/user/UserController.java
+++ src/main/java/com/barreto/todolist/user/UserController.java
@@ -5,10 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -31,5 +30,16 @@ public class UserController {
         var userCreateOutDTO = new UserCreateOutDTO(userCreated);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreateOutDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity find(@PathVariable UUID id) {
+        var user = this.userRepository.findById(id);
+
+        if (user.isEmpty()) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+
+        var userCreateOutDTO = new UserCreateOutDTO(user.get());
+
+        return ResponseEntity.ok(userCreateOutDTO);
     }
 }
